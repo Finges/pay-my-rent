110599d5d7f9241459633212852db12ed65c5440
Chris Lee <finges@gmail.com>
2013-05-02 11:01:03 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PayMyRent::Application.config.secret_token = '11657384761c8fc7a2d17fd220b43d0b772f931c797410275bef38b6bbe657167d36f037204f204aed1a2a071736e84b8ec059d841d6c5d823249b316cf30f79'
